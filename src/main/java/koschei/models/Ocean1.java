2971bfef2105a3ef82c1f98e09aa9ac1eb3e5c91
Suprimi4 <1LoveTwitch1@gmail.com>
2025-02-25 21:44:20 +0300

    init commit

--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -6,8 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class Ocean1 {
 
+
+    private final Island2 island;
+
     @Autowired
-    private Island2 island;
+    public Ocean1(Island2 island) {
+        this.island = island;
+    }
 
     @Override
     public String toString() {
