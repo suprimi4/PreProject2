4de5db2283f9647e774a92ae1793bdecb8ff2313
Suprimi4 <1LoveTwitch1@gmail.com>
2025-02-25 22:52:49 +0300

    third fix

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Egg6 {
     private final Needle7 needle7;
 
-    @Autowired
+
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
