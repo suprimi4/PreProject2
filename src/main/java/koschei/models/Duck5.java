de8c76c0deba7c4b9827dd4161eae47aebfcd9a8
Suprimi4 <1LoveTwitch1@gmail.com>
2025-02-25 22:33:05 +0300

    second fix task

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 public class Duck5 {
     private final Egg6 egg6;
 
-    @Autowired
+
     public Duck5(Egg6 egg6) {
         this.egg6 = egg6;
     }
